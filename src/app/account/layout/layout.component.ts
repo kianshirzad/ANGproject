88ac824654e071cf6e2b73d0199e745cc370196f
esmaeel motaghi <esmaeel.m@live.com>
2024-05-15 09:46:59 +0330

    .

--- src/app/account/layout/layout.component.ts
+++ src/app/account/layout/layout.component.ts
@@ -2,12 +2,27 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 import { DataService } from '../../data.service';
 
 @Component({
-  selector : 'LayoutDashboard',
+  selector: 'LayoutDashboard',
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.Default,
   encapsulation: ViewEncapsulation.None
 })
 export class LayoutComponent {
-constructor(public data:DataService){}
+  constructor(public data: DataService) { }
+
+  select(item: any) {
+    this.routes.forEach(c=>c.selected = false)
+    item.selected = true;
+  }
+  routes = [
+    { selected: false, icon: 'home', route: '/account/dashboard', title: 'داشبورد' },
+    { selected: false, icon: 'account', route: '/account/profile', title: 'اطلاعات کاربر' },
+    { selected: false, icon: 'lock', route: '/account/changepassword', title: 'تعیین رمز عبور ثابت' },
+    { selected: false, icon: 'basket', route: '/account/dashboard', title: 'سفارش ها' },
+    { selected: false, icon: 'heart', route: '/account/orders', title: 'لیست مورد علاقه' },
+    { selected: false, icon: 'map', route: '/account/addressess', title: 'آدرس های شما' },
+    { selected: false, icon: 'message', route: '/account/titlesindashboardLayout', title: 'تیکت ها و پیغام ها' },
+    { selected: false, icon: 'dollar', route: '/account/payments', title: 'پرداخت ها و حسابداری' },
+  ]
 }
